81704365eb2a94232d38823a1eeec260ebd1a51e
Valenti Gamez <vgr.gamez@gmail.com>
2019-10-01 21:33:22 +0200

    Added base to jwt.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 *NOTA Esta documentación está en castellano por motivos de productividad dada la comunidad que la desarrolla. En cuanto salga una versión beta toda documentación va a estar también en Inglés.
 
+##Instalación
+composer install
+
+Seguir los siguientes pasos para instalar jwt:
+https://api-platform.com/docs/core/jwt/
+
+Shortcut:
+
+`set -e`
+
+`apk add openssl`
+
+`mkdir -p config/jwt`
+
+Configurar variable JWT_PASSPHRASE en el fichero .env.local
+
+`jwt_passhrase=$(grep ''^JWT_PASSPHRASE='' .env.local | cut -f 2 -d ''='')`
+
+`echo "$jwt_passhrase" | openssl genpkey -out config/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096`
+
+`echo "$jwt_passhrase" | openssl pkey -in config/jwt/private.pem -passin stdin -out config/jwt/public.pem -pubout`
+
+`chmod 755 config/jwt/public.pem`
+
+`chmod 750 config/jwt/private.pem`
+
+Si es necesario se debe poner un grupo que el usuario que ejecuta el servidor tenga permisos en los dos ficheros anteriores.
---
+++ src/Controller/ApiController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use CTIC\App\Account\Domain\Account;
+use CTIC\App\Company\Domain\Company;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use CTIC\App\User\Domain\User;
+use CTIC\App\User\Application\CreateUser;
+use CTIC\App\User\Domain\Command\UserCommand;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class ApiController extends AbstractController
+{
+    public function register(Request $request, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+
+        if (!$username || !$password) {
+            return new Response('_username or _password not found', Response::HTTP_BAD_REQUEST);
+        }
+
+        $accountRepository = $em->getRepository(Account::class);
+        $account = $accountRepository->find(1);
+        $companyRepository = $em->getRepository(Company::class);
+        $company = $companyRepository->find(1);
+
+        $userCommand = new UserCommand();
+        $userCommand->name = $username;
+        $userCommand->username = $username;
+        $userCommand->enabled = true;
+        $userCommand->permission = 3;
+        $userCommand->account = $account;
+        $userCommand->defaultCompany = $company;
+
+        $user = CreateUser::create($userCommand);
+
+        $user->password = $encoder->encodePassword($user, $password);
+
+        $validator->validate($user);
+        $em->persist($user);
+        $em->flush();
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    public function api()
+    {
+        return new Response(sprintf('Logged in as %s. Role/s %s', $this->getUser()->getUsername(), implode(',', $this->getUser()->getRoles())));
+    }
+}
